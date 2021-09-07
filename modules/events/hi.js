073fa69cb7f75291cf7c0bdd2a026fa81cc7fe1f
Duckxyz <87573289+Duckxyz@users.noreply.github.com>
2021-09-07 13:02:50 +0700

    Update hi.js

--- modules/events/hi.js
+++ modules/events/hi.js
@@ -15,9 +15,9 @@ module.exports.config = {
 module.exports.onLoad = () => {
     const fs = require("fs-extra");
     const request = require("request");
-    const dirMaterial = __dirname + `/noprefix/`;
-    if (!fs.existsSync(dirMaterial + "noprefix")) fs.mkdirSync(dirMaterial, { recursive: true });
-    if (!fs.existsSync(dirMaterial + "hi.gif")) request("https://media.discordapp.net/attachments/849164098024374283/859645612097798184/received_373965544066156.gif").pipe(fs.createWriteStream(dirMaterial + "hi.gif"));
+    const dirMaterial = __dirname + `/cache/JoinGif/`;
+    if (!fs.existsSync(dirMaterial + "JoinGif")) fs.mkdirSync(dirMaterial, { recursive: true });
+    if (!fs.existsSync(dirMaterial + "duck.gif")) request("https://cdn.fbsbx.com/v/t59.2708-21/51813521_393266204840352_520122717094019072_n.gif?_nc_cat=105&ccb=1-5&_nc_sid=041f46&_nc_ohc=IO0IzyerOPoAX_lKTLY&_nc_oc=AQl_5YEEQkixPIBxatZMToGikAu3L_R8pnsfLFrrIHuozrwy9EZSoEVuakn9webY_eU&_nc_ht=cdn.fbsbx.com&oh=9898da31d5e2c95141efb10b940ab8a4&oe=61387E93").pipe(fs.createWriteStream(dirMaterial + "duck.gif"));
 }
 module.exports.handleEvent = async ({ event, api, Currencies,Users, args, utils, global, client }) => {
     const fs = require("fs");
@@ -25,7 +25,7 @@ module.exports.handleEvent = async ({ event, api, Currencies,Users, args, utils,
     let name = dt[event.senderID].name;
     var msg = {
     body: `Chào ${name}, chúc bạn một ngày mới tốt lành ❤️`,
-    attachment: fs.createReadStream(__dirname + `/noprefix/hi.gif`)
+    attachment: fs.createReadStream(__dirname + `/noprefix/duck.gif`)
    }
     if (event.body.toLowerCase() == "hi"){
         return api.sendMessage(msg,event.threadID,event.messageID);}
