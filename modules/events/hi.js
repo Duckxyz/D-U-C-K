5c095717a2fce182b57856e4f09de49da0274872
Duckxyz <87573289+Duckxyz@users.noreply.github.com>
2021-09-08 17:29:46 +0700

    Update hi.js

--- modules/events/hi.js
+++ modules/events/hi.js
@@ -15,8 +15,8 @@ module.exports.config = {
 module.exports.onLoad = () => {
     const fs = require("fs-extra");
     const request = require("request");
-    const dirMaterial = __dirname + `/cache/JoinGif/`;
-    if (!fs.existsSync(dirMaterial + "JoinGif")) fs.mkdirSync(dirMaterial, { recursive: true });
+    const dirMaterial = __dirname + `/cache/joinGif/`;
+    if (!fs.existsSync(dirMaterial + "joinGif")) fs.mkdirSync(dirMaterial, { recursive: true });
     if (!fs.existsSync(dirMaterial + "duck.gif")) request("https://cdn.fbsbx.com/v/t59.2708-21/51813521_393266204840352_520122717094019072_n.gif?_nc_cat=105&ccb=1-5&_nc_sid=041f46&_nc_ohc=IO0IzyerOPoAX_lKTLY&_nc_oc=AQl_5YEEQkixPIBxatZMToGikAu3L_R8pnsfLFrrIHuozrwy9EZSoEVuakn9webY_eU&_nc_ht=cdn.fbsbx.com&oh=9898da31d5e2c95141efb10b940ab8a4&oe=61387E93").pipe(fs.createWriteStream(dirMaterial + "duck.gif"));
 }
 module.exports.handleEvent = async ({ event, api, Currencies,Users, args, utils, global, client }) => {
